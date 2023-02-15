efbbd2a6ad12c548fa6ea0e06d3490ac6909b792
Stepkin Alexander <stepkin@speechpro.com>
2023-02-15 21:30:59 +0300

    dynamic route added

---
+++ pages/iphone/iphone-[name].vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<p>iphone {{ name }}</p>
+	</div>
+</template>
+
+<script setup>
+	
+		const route = useRoute();
+		
+		const name = computed(() => {
+			return route.params.name.replaceAll("-"," ");
+		})
+
+</script>
+
+<style scoped>
+
+</style>
